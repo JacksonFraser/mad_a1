ccabadddff4b50961643aacc06a185aff10343a8
Forest Xiang <s3530377@w8408826.cs.rmit.edu.au>
2017-08-06 13:30:03 +1000

    ContactDataManager works

--- app/src/main/java/com/example/s3529589/mad_a1/MainActivity.java
+++ app/src/main/java/com/example/s3529589/mad_a1/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.s3529589.mad_a1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    protected static final int PICK_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         addFriendBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                nextActivity(v);
+                addFriendActivity(v);
             }
         });
     }
 
-    public void nextActivity(View view){
-        Intent myIntent = new Intent(this, AddFriendActivity.class);
-        this.startActivityForResult(myIntent, 1);
+
+    public void addFriendActivity(View view){
+        this.setContentView(R.layout.add_friend);
+
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, PICK_CONTACTS);
     }
 }
 
