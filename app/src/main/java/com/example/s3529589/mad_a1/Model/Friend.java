0d0f4b5cbd41697917d72744a08da17bed4ac159
Jackson Fraser <s3529589@w8408843.cs.rmit.edu.au>
2017-08-06 15:23:25 +1000

    Changes

---
+++ app/src/main/java/com/example/s3529589/mad_a1/Model/Friend.java
@@ -0,0 +1,17 @@
+package com.example.s3529589.mad_a1.Model;
+
+
+import android.widget.DatePicker;
+
+public class Friend {
+    private String id;
+    private String name;
+    private String email;
+    private DatePicker birthday;
+
+    public Friend(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+}
--- app/src/main/java/com/example/s3529589/mad_a1/Model/MainActivity.java
+++ app/src/main/java/com/example/s3529589/mad_a1/Model/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.s3529589.mad_a1.Model;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.example.s3529589.mad_a1.Controller.AddFriendController;
 import com.example.s3529589.mad_a1.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private List<Friend> friendList = new ArrayList<Friend>();
+    private String LOG_TAG = this.getClass().getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
         View addFriendBtn = findViewById(R.id.addFriends);
         addFriendBtn.setOnClickListener(new AddFriendController(this));
+    }
+
+    public void addToFriendList(Friend f){
+        friendList.add(f);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                ContactDataManager contactsManager = new ContactDataManager(this, data);
+                String name = "";
+                String email = "";
+                try {
+                    name = contactsManager.getContactName();
+                    email = contactsManager.getContactEmail();
+                } catch (ContactDataManager.ContactQueryException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                }
+            }
+        }
     }
 
 }
