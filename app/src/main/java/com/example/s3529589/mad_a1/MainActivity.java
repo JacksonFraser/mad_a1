dcfd5850e2cc99f672b7e1c36f3ece0840fd750b
Forest Xiang <forest@Forests-MacBook-Pro.local>
2017-08-06 11:50:38 +1000

    Layout

--- app/src/main/java/com/example/s3529589/mad_a1/MainActivity.java
+++ app/src/main/java/com/example/s3529589/mad_a1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.s3529589.mad_a1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addFriendBtn = (Button) findViewById(R.id.addFriends);
+
+        addFriendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Add Friend");
+            }
+        });
     }
 }
