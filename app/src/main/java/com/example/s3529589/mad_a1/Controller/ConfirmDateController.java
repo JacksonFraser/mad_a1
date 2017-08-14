0cf905961bdb84905e0849ea858bf8fc7cd074b0
jacksonfraser <s3529589@student.rmit.edu.au>
2017-08-14 17:18:44 +1000

    commit

--- app/src/main/java/com/example/s3529589/mad_a1/Controller/ConfirmDateController.java
+++ app/src/main/java/com/example/s3529589/mad_a1/Controller/ConfirmDateController.java
@@ -1,6 +1,7 @@
 package com.example.s3529589.mad_a1.Controller;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 
@@ -23,10 +24,18 @@ public class ConfirmDateController implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         DatePicker d = (DatePicker) datePickerActivity.findViewById(R.id.datePicker);
-        datePickerActivity.setDatePicker(d);
+        String birthday =
+                 String.valueOf(d.getDayOfMonth())
+                +String.valueOf(d.getMonth())
+                +String.valueOf(d.getYear());
+
+        Bundle b = new Bundle();
+        b.putString("name", datePickerActivity.getName());
+        b.putString("email", datePickerActivity.getEmail());
+        b.putString("birthday", birthday);
         Intent it = new Intent(datePickerActivity, FriendMenuActivity.class);
-        it.putExtra(datePickerActivity.getDatePicker().toString(), true);
-       // .startActivityForResult(it, 10);
+        it.putExtra("friendDetails",b);
+         FriendMenuActivity.startActivityForResult(it, 10);
     }
 
 }
