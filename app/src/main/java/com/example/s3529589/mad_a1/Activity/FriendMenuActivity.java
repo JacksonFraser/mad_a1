7437880f2c80ff7f576f69e3feb43331ea364c98
jacksonfraser <s3529589@student.rmit.edu.au>
2017-08-16 18:08:35 +1000

    Fixed some errors

--- app/src/main/java/com/example/s3529589/mad_a1/Activity/FriendMenuActivity.java
+++ app/src/main/java/com/example/s3529589/mad_a1/Activity/FriendMenuActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import com.example.s3529589.mad_a1.Controller.AddFriendController;
 import com.example.s3529589.mad_a1.Model.ContactDataManager;
+import com.example.s3529589.mad_a1.Model.DateSingleton;
+import com.example.s3529589.mad_a1.Model.Friend;
 import com.example.s3529589.mad_a1.R;
 
 public class FriendMenuActivity extends Activity{
@@ -23,40 +25,10 @@ public class FriendMenuActivity extends Activity{
         View addBtn = findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new AddFriendController(this));
 
-<<<<<<< HEAD:app/src/main/java/com/example/s3529589/mad_a1/Activity/FriendMenuActivity.java
-        // Recieve intent from DatePickerActivity
-        Intent incomingIntent = getIntent();
-        String date = incomingIntent.getStringExtra("date");
-        System.out.println("Printing date in FriendMenuActivity: " + date);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100) {
-            if (resultCode == RESULT_OK) {
-                ContactDataManager contactsManager = new ContactDataManager(this, data);
-                String name = "";
-                String email = "";
-                try {
-                    name = contactsManager.getContactName();
-                    email = contactsManager.getContactEmail();
-
-                    // Immediately prompt user to choose birthday after selecting contact
-                    Intent it = new Intent(this, DatePickerActivity.class);
-                    this.startActivity(it);
-
-                    // Finish activity
-                    finish();
-
-                } catch (ContactDataManager.ContactQueryException e) {
-                }
-            }
-=======
         for(Friend f : DateSingleton.getInstance().getFriendList()){
             System.out.println(f.getName());
             System.out.println(f.getEmail());
             System.out.println(f.getBirthdate());
->>>>>>> testBranch:app/src/main/java/com/example/s3529589/mad_a1/Model/FriendMenuActivity.java
         }
     }
 }
--- app/src/main/java/com/example/s3529589/mad_a1/Controller/AddFriendController.java
+++ app/src/main/java/com/example/s3529589/mad_a1/Controller/AddFriendController.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.view.View;
-<<<<<<< HEAD
-import com.example.s3529589.mad_a1.Activity.FriendMenuActivity;
-=======
 
-import com.example.s3529589.mad_a1.Model.DatePickerActivity;
-import com.example.s3529589.mad_a1.Model.FriendMenuActivity;
->>>>>>> testBranch
+import com.example.s3529589.mad_a1.Activity.DatePickerActivity;
+import com.example.s3529589.mad_a1.Activity.FriendMenuActivity;
+
 
 
 public class AddFriendController implements View.OnClickListener {
