031b55bf33edf222ecf5a1d38afe57c05e7061ae
Jackson Fraser <s3529589@w8408846.cs.rmit.edu.au>
2017-08-26 12:57:41 +1000

    update

--- app/src/main/java/com/example/s3529589/mad_a1/Controller/FriendMenuLongClickController.java
+++ app/src/main/java/com/example/s3529589/mad_a1/Controller/FriendMenuLongClickController.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.s3529589.mad_a1.Model.CustomArrayArrayAdapter;
 import com.example.s3529589.mad_a1.Model.DataSingleton;
@@ -62,12 +64,47 @@ public class FriendMenuLongClickController implements View.OnLongClickListener {
         }
     }
     public void editFriend(){
+        String choices[] = {"Cancel","Confirm"};
         LayoutInflater factory = LayoutInflater.from(customArrayArrayAdapter.getContext());
         final View textEntryView = factory.inflate(R.layout.edit_friend, null);
         AlertDialog.Builder alert = new AlertDialog.Builder(customArrayArrayAdapter.getContext());
         alert.setTitle("Choose an option");
         alert.setView(textEntryView);
 
+
+
+
+        alert.setItems(choices, new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch(which){
+                    case 0 :
+
+                        break;
+                    case 1 :
+                        editFriendDetails(id);
+                        break;
+                }
+            }
+        });
+
+
+
         alert.show();
+    }
+    private void editFriendDetails(int id, String name, String email){
+        try {
+            for(Friend f : DataSingleton.getInstance().getFriendList()){
+                if(f.getId() ==  id) {
+                    f.setName(name);
+                    f.setEmail(email);
+                    customArrayArrayAdapter.notifyDataSetChanged();
+                }
+            }
+
+        } catch(Exception e ){
+
+        }
     }
 }
