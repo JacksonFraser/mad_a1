a67b745b27dddf6bc4646528105c4d7d09a6f15e
Jackson Fraser <s3529589@w8408846.cs.rmit.edu.au>
2017-08-30 16:08:17 +1000

    Added meeting time

--- app/src/main/java/com/example/s3529589/mad_a1/Activity/meetingActivities/ScheduleMeetingActivity.java
+++ app/src/main/java/com/example/s3529589/mad_a1/Activity/meetingActivities/ScheduleMeetingActivity.java
@@ -10,6 +10,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.example.s3529589.mad_a1.Controller.meetingControllers.CustomDatePickerDialogController;
+import com.example.s3529589.mad_a1.Model.CustomTimePickDialogController;
 import com.example.s3529589.mad_a1.Model.DataSingleton;
 import com.example.s3529589.mad_a1.Model.Friend;
 import com.example.s3529589.mad_a1.R;
@@ -24,20 +25,31 @@ public class ScheduleMeetingActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule_meeting);
 
+        //Meeting title
         EditText meetingTitleView = (EditText) findViewById(R.id.meeting_title);
         String meetingTitle = meetingTitleView.getText().toString();
 
+
+        //Meeting start date
         View startDateBtn = findViewById(R.id.start_date_btn);
         final EditText meetingStartDateET = (EditText) findViewById(R.id.start_time);
         String meetingStartDate = meetingStartDateET.getText().toString();
+        startDateBtn.setOnClickListener(new CustomDatePickerDialogController(this,meetingStartDateET));
 
+        //Meeting end date
         View endDateBtn = findViewById(R.id.end_date_btn);
         final EditText meetingEndDateET = (EditText) findViewById(R.id.end_time);
         String meetingEndDate = meetingEndDateET.getText().toString();
-
-        startDateBtn.setOnClickListener(new CustomDatePickerDialogController(this,meetingStartDateET));
-
         endDateBtn.setOnClickListener(new CustomDatePickerDialogController(this,meetingEndDateET));
+
+        //Meeting start time
+        View startTimeBtn = findViewById(R.id.start_time_meeting_button);
+        startTimeBtn.setOnClickListener(new CustomTimePickDialogController(this,meetingStartDateET));
+
+        //Meeting end time
+        View endTimeBtn = findViewById(R.id.end_time_meeting_button);
+        endTimeBtn.setOnClickListener(new CustomTimePickDialogController(this,meetingEndDateET));
+
 
 
 
